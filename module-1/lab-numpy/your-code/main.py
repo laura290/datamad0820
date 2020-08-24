24b5a4626b57913e20f8c46a55bfafffd0cd49e0
laura <laura_larrad_perez@hotmail.com>
2020-08-24 23:10:13 +0200

    Ejercicios resueltos

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -10,6 +10,8 @@ print(np.version.version)
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 a = np.random.random((2,3,5))
+"Array de enteros entre -N y M:np.random.randint(-10, 10, size=[3,2])"
+"Array 3D de decimales entre 0 y N:np.random.uniform(10, size=[2,2,2])"
 
 
 
@@ -30,18 +32,17 @@ print(b)
 #7. Do a and b have the same size? How do you prove that in Python code?
 print(a.size)
 print(b.size)
-#En este caso ambos tienen en mismo tamaño,ya que son dos matrices distintas,pero con los mismos elementos.
+"Ambas tienen la misma ya que tienen el mismo numero de elementos aunque las dimensiones de la matriz sean diferentes."
 
 
 
 #8. Are you able to add a and b? Why or why not?
 
-#No se podrian sumar tal cual,ya que cambiaría de dimension la matriz,se podrian concatenar.Se podría sumar tal cual en el caso de ser listas.
+"No se podrian sumar tal cual,ya que cambiaría de dimension la matriz,se podrian concatenar.Se podría sumar tal cual en el caso de ser listas."
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 c = b.reshape(2,3,5)
-c = b.T
 print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
@@ -50,43 +51,59 @@ d  = sum(a,c)
 print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
+print (a)
+print(d)
+"Se trata de trata de la misma matriz,habiendole añadido una matriz de unos(b),pero haciendo un reshape para que tuviera las mismas dimensiones que a."
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e = a * c
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
 
-
+"Son iguales a (a),ya que multiplicas por una función que es 1"
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_min = np.min(d)
+print(d_min)
+d_max = np.max(d)
+print(d_max)
+d_mean = np.mean (d)
+print(d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+f = np.empty(d.shape)
+print(f)
 
-
-
-
-"""
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
-If a value equals to d_mean, assign 50 to the corresponding value in f.
-Assign 0 to the corresponding value(s) in f for d_min in d.
-Assign 100 to the corresponding value(s) in f for d_max in d.
-In the end, f should have only the following values: 0, 25, 50, 75, and 100.
-Note: you don't have to use Numpy in this question.
-"""
+#If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+#f a value equals to d_mean, assign 50 to the corresponding value in f.
+#Assign 0 to the corresponding value(s) in f for d_min in d.
+#Assign 100 to the corresponding value(s) in f for d_max in d.
+#In the end, f should have only the following values: 0, 25, 50, 75, and 100.
+#Note: you don't have to use Numpy in this question.
+g = d.tolist()
+h = []
+for each_list in g:
+    for i in each_list:
+        if (i > d_min) & (i < d_mean):
+            i==25
+        elif(i > d_mean) & (i < d_max):
+            i==75
+        elif(i==d_mean):
+            i==50
+        elif(i==d_min):
+            i==0
+        else:
+            (i==d_max)
+            i==100
+        h.append(i)
+f =np.asarray(h)
 
-
-
-
-"""
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
@@ -119,4 +136,4 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
-"""
+t = ["A", "B", "C", "D", "E"]
